c0a733fe61587567d87da5062c80b7c741a38175
Yuri Miranda <yuricmiranda25@outlook.com>
2026-09-06 22:54:46 -0300

    Aula de Condicional05

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05Condicionais05.java
@@ -0,0 +1,48 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05Condicionais05 {
+    public static void main(String[] args) {
+        byte dia = -1;
+        switch (dia) {
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda");
+                break;
+            case 3:
+                System.out.println("Terça");
+                break;
+            case 4:
+                System.out.println("Quarta");
+                break;
+            case 5:
+                System.out.println("Quinta");
+                break;
+            case 6:
+                System.out.println("Sextouuu");
+                break;
+            case 7:
+                System.out.println("Sabado");
+                break;
+            default:
+                System.out.println("OPÇÃO INVALIDAAAAA");
+                break;
+
+        }
+
+        char sexo = 'M';
+
+            switch (sexo) {
+                case 'm':
+                    System.out.println("Masculino");
+                    break;
+                case 'F':
+                    System.out.println("Feminino");
+                    break;
+                default:
+                    System.out.println("opção invalida");
+            }
+
+    }
+}
