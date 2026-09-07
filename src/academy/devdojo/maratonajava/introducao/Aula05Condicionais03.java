00469fa4e9a44263a37dcc82866f4f2973a6bf51
Yuri Miranda <yuricmrianda25@outlook.com>
2026-09-06 21:15:53 -0300

    condicionais03

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05Condicionais03.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05Condicionais03 {
+    public static void main(String[] args) {
+        double sal = 5001;
+        String msgNDoar = "Não vai doar (vai faltar grana no fim do mês)";
+        String msgDoar = "Pode doar (ta com grana ein pai) >< ";
+        String res = sal > 5000 ? msgDoar : msgNDoar;
+
+        if (sal >= 5000) {
+            res = msgDoar;
+        } else {
+            res = msgNDoar;
+        }
+        System.out.println(res);
+
+
+
+
+    }
+}
